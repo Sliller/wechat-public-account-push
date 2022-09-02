31aff87a948021e45fee6bf61d4372a031f7268b
Ssspecial_river <31638802+Sliller@users.noreply.github.com>
2022-09-02 10:55:22 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -4,13 +4,13 @@ export const config = {
      */
 
     // 公众号APP_ID
-    APP_ID: "",
+    APP_ID: "wx6f4b205d43b12546",
 
     // 公众号APP_SECRET
-    APP_SECRET: "",
+    APP_SECRET: "01de59ca24e1d566b8d5daff78e1c882",
 
     // 模板消息id
-    TEMPLATE_ID: "",
+    TEMPLATE_ID: "ZJOjy-7EHBWfNmv7aRSSZxneTi_RXNqnPSgdAmYVyIs",
 
     // 回调消息模板id, 用来看自己有没有发送成功的那个模板
     CALLBACK_TEMPLATE_ID: "",
@@ -19,19 +19,19 @@ export const config = {
     /**
      * 接收公众号消息的微信号，如果有多个，需要在[]里用英文逗号间隔，例如
      * [
-     *   {name: '老婆0', id: "ohxOO6VskYv0Kuf-ywIZVRMpuXhk"}, 
+     *   {name: '老婆0', id: "o1xLt6t9kTOvRwZh0cgyF1oGWfgo"}, 
      *   {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"},
      * ]
      */
     USERS: [
-      {name: '老婆0', id: "1hxOO6VskYv0Kuf-y你懂的,填错狗头打烂VRMpuXhk"}, 
+      {name: '主人', id: "o1xLt6t9kTOvRwZh0cgyF1oGWfgo"}, 
       {name: '老婆1', id: "2hxOO6VskYv0Kuf-y你懂的,填错狗头打烂RMpuXhk"}, 
       {name: '老婆2', id: "3hxOO6VskYv0Kuf-yw你懂的,填错狗头打烂2RMpuXhk"}, 
       {name: '老婆3', id: "4hxOO6VskYv0Kuf-ywI你懂的,填错狗头打烂MpuXhk"}, 
     ],
     // 接收成功回调消息的微信号，（一般来说只填自己的微信号, name填不填无所谓）
     CALLBACK_USERS: [
-      {name: '自己',id: "5hxOO6VskYv你懂的,填错狗头打烂-ywIZVRMpuXhk"}, 
+      {name: '自己',id: "joshualjz"}, 
     ],
      
     /**
